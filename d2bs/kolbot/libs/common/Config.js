2d5e5285479d166506a5fa32efc444a958617ea7
cybergodjsp <gamemasterr789@yahoo.com>
2015-10-21 17:53:14 -0400

    1

--- d2bs/kolbot/libs/common/Config.js
+++ d2bs/kolbot/libs/common/Config.js
@@ -183,7 +183,7 @@ var Config = {
 
 	ItemInfo: false,
 	ItemInfoQuality: [],
-	DropStats =  {
+	DropStats:  {
       Enable: true,      // Switch on/off the function - bool.
       Quality: [],   // the item quality to log (1 - lowquality, 2 - normal, 3 - superior, 4 - magic, 5 - set, 6 - rare, 7 - unique, 8 - crafted)
       AutoLog: true      // update the readable file each run, if false press "numpad dot" to update it.
